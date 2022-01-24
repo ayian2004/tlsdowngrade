8172afbe4c201ba14fa1aa3e28889e44e53d52d9
Alkis Yian <alkiviadis.giannakoulias@eurodyn.com>
2022-01-24 08:14:24 +0200

    Update and rename evil-program.sh to victims-program.sh

--- victim/scripts/victims-program.sh
+++ victim/scripts/victims-program.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
-MALICIOUS_SERVER="malicious-server.eurodyn.com" # this is actually just a deom web server!
+CORPORATE_SERVER="corporate-server.eurodyn.com" # this is actually just a demo web server!
 
 while true
 do
-    echo "Initiating connection to malicious server $MALICIOUS_SERVER ..."
+    echo "Initiating connection to corporate server $CORPORATE_SERVER ..."
 
     # if not reachable, try again
-    ping -c1 -W1 $MALICIOUS_SERVER 1>/dev/null 2>&1
+    ping -c1 -W1 $CORPORATE_SERVER 1>/dev/null 2>&1
 
     if [ $? -eq 0 ]; then
 
         echo "Server reachable, performing handshake..."
-        ciphers=$(nmap --script ssl-enum-ciphers -p 443 $MALICIOUS_SERVER)
+        ciphers=$(nmap --script ssl-enum-ciphers -p 443 $CORPORATE_SERVER)
         hasStrongestCipher=$(echo "$ciphers" | grep 'TLS_RSA_WITH_AES_256_CBC_SHA' | wc -l)
 
         echo "Available ciphers:"
-        nmap --script ssl-enum-ciphers -p 443 $MALICIOUS_SERVER | grep -A 3 "TLSv1.2" | cut -c 5-
+        nmap --script ssl-enum-ciphers -p 443 $CORPORATE_SERVER | grep -A 3 "TLSv1.2" | cut -c 5-
 
         if [ "$hasStrongestCipher" -eq "1" ]; then
-            echo "Using stronger cipher suite (AES 256)...              <- your downgrade attack failed !"
+            echo "Using stronger cipher suite (AES 256)...              <- Our downgrade attack failed !"
             echo "Exchanging secret stuff on this very secure channel..."
             echo "Done."
         else
